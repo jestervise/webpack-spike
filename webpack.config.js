9c1af5eee0d2dc28f75b41740d668f98058b78b6
MJ Chang <none>
2021-09-24 01:31:42 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+
+const path = require('path')
+module.exports = {
+    "entry" : {index: path.resolve(__dirname,"source","index.js")},
+    "output":{ path: path.resolve(__dirname, "build"), filename: "main.js" }
+}
